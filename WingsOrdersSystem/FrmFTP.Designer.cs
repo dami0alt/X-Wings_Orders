cf760e4f96a4197196be594603b38cc8b15acc23
Axololt9597 <axel.fajardo@sarria.salesians.cat>
2026-02-18 20:50:55 +0100

    feat: formulari ftp(#21)
    * Feature/FTP_XML
    
    Creacion de XML
    
    * Update/csproj
    
    Nombre
    
    * Feature/FrmFTP_Classes
    
    Terminado tanto el formulario FTP como sus Clases
    
    * Update/Program

---
+++ WingsOrdersSystem/FrmFTP.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WingsOrdersSystem
+{
+	partial class FrmFTP
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnCheck = new System.Windows.Forms.Button();
+			this.txtLog = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnCheck
+			// 
+			this.btnCheck.Location = new System.Drawing.Point(316, 135);
+			this.btnCheck.Name = "btnCheck";
+			this.btnCheck.Size = new System.Drawing.Size(75, 23);
+			this.btnCheck.TabIndex = 0;
+			this.btnCheck.Text = "Check";
+			this.btnCheck.UseVisualStyleBackColor = true;
+			this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+			// 
+			// txtLog
+			// 
+			this.txtLog.Location = new System.Drawing.Point(420, 135);
+			this.txtLog.Multiline = true;
+			this.txtLog.Name = "txtLog";
+			this.txtLog.Size = new System.Drawing.Size(334, 303);
+			this.txtLog.TabIndex = 1;
+			// 
+			// FrmFTP
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.txtLog);
+			this.Controls.Add(this.btnCheck);
+			this.Name = "FrmFTP";
+			this.Text = "FrmFTP";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnCheck;
+		private System.Windows.Forms.TextBox txtLog;
+	}
+}
---
+++ WingsOrdersSystem/FtpService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace WingsOrdersSystem
+{
+	class FtpService
+	{
+
+		readonly FtpConfig Config;
+
+		public FtpService(FtpConfig config)
+		{
+			Config = config;
+		}
+
+		// 🔹 Listar archivos del FTP
+		public string[] ListFiles()
+		{
+			FtpWebRequest request = (FtpWebRequest)
+				WebRequest.Create($"ftp://{Config.Host}{Config.RemoteRoot}");
+
+			request.Method = WebRequestMethods.Ftp.ListDirectory;
+			request.Credentials = new NetworkCredential(Config.User, Config.Pass);
+
+			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+			Stream responseStream = response.GetResponseStream();
+			StreamReader reader = new StreamReader(responseStream);
+
+			string data = reader.ReadToEnd();
+
+			reader.Close();
+			response.Close();
+
+			return data.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+		}
+
+		public void DownloadFile(string fileName)
+		{
+			string ftpUrl = $"ftp://{Config.Host}{Config.RemoteRoot}/{fileName}";
+
+			FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
+
+			request.Method = WebRequestMethods.Ftp.DownloadFile;
+			request.Credentials = new NetworkCredential(Config.User, Config.Pass);
+
+			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+			Stream responseStream = response.GetResponseStream();
+
+			if (!Directory.Exists(Config.LocalFolder))
+				Directory.CreateDirectory(Config.LocalFolder);
+
+			string localPath = Path.Combine(Config.LocalFolder, fileName);
+
+			using (FileStream fs = new FileStream(localPath, FileMode.Create))
+			{
+				responseStream.CopyTo(fs);
+			}
+
+			response.Close();
+		}
+
+		public void MoveToProcessed(string fileName)
+		{
+			string ftpUrl = $"ftp://{Config.Host}{Config.RemoteRoot}/{fileName}";
+
+			FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
+
+			request.Method = WebRequestMethods.Ftp.Rename;
+			request.Credentials = new NetworkCredential(Config.User, Config.Pass);
+
+			request.RenameTo = $"{Config.ProcessedFolder}/{fileName}";
+
+			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+			response.Close();
+		}
+
+	}
+}

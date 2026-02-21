e84c3d0823692eaf7f9ffc35836a6251c04db979
MarCanLan <martina.canamares@sarria.salesians.cat>
2026-02-21 18:17:04 +0100

    feat: creado el frmCrystal (#23)
    * creado el frmCrystal
    
    solo hace falta que el fichero edi lo llame y le mande los id's
    
    * Update CrystalReport.csproj
    
    * fix: connection
    
    solucion app.config y buil-validation
    
    * Update X-Wings_Orders.sln
    
    añadido el EndProject
    
    * Update build-validation.yml
    
    fix: connection
    
    * fix: app.config
    
    quitado el duplicado
    
    * fix: connection min
    
    * fix:dependencias
    
    he creado una carpeta para añadir las dependecias del crystal y que las coja de allí
    
    * fix: referencias
    
    no referencias

---
+++ CrystalReport/frmCrystalReport.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace CrystalReport
+{
+    partial class frmCrystalReport
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crvJobHunters = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.JobHuntersReport1 = new CrystalReport.JobHuntersReport();
+            this.SuspendLayout();
+            // 
+            // crvJobHunters
+            // 
+            this.crvJobHunters.ActiveViewIndex = 0;
+            this.crvJobHunters.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crvJobHunters.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crvJobHunters.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crvJobHunters.Location = new System.Drawing.Point(0, 0);
+            this.crvJobHunters.Margin = new System.Windows.Forms.Padding(2);
+            this.crvJobHunters.Name = "crvJobHunters";
+            this.crvJobHunters.ReportSource = this.JobHuntersReport1;
+            this.crvJobHunters.Size = new System.Drawing.Size(711, 674);
+            this.crvJobHunters.TabIndex = 0;
+            this.crvJobHunters.ToolPanelWidth = 133;
+            // 
+            // frmCrystalReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 674);
+            this.Controls.Add(this.crvJobHunters);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmCrystalReport";
+            this.Text = "Crystal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmCrystalReport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crvJobHunters;
+        private JobHuntersReport JobHuntersReport1;
+    }
+}
+
---
+++ CrystalReport/frmCrystalReport.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+
+namespace CrystalReport
+{
+    public partial class frmCrystalReport : Form
+    {
+        private List<int> _idOrder;
+        public frmCrystalReport(List<int> idOrder)
+        {
+            InitializeComponent();
+            _idOrder = idOrder;
+        }
+
+        private void frmCrystalReport_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ReportDocument cryRpt = new ReportDocument();
+                string pathReport = System.IO.Path.Combine(Application.StartupPath, "JobHuntersReport.rpt");
+                cryRpt.Load(pathReport);
+
+                Credentials(cryRpt);
+
+                if (_idOrder != null && _idOrder.Count > 0)
+                {
+                    string ids = string.Join(",", _idOrder);
+                    cryRpt.RecordSelectionFormula = "{Orders.idOrder} IN [" + ids + "]";
+                }
+
+                crvJobHunters.ReportSource = cryRpt;
+                crvJobHunters.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void Credentials(ReportDocument cryRpt)
+        {
+            string cnx;
+            ConnectionStringSettings conf = ConfigurationManager.ConnectionStrings["SecureCoreG2Configuration"];
+
+            if (conf == null) return;
+            cnx = conf.ConnectionString;
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(cnx);
+            string server = builder.DataSource;
+            string database = builder.InitialCatalog;
+            string user = builder.UserID;
+            string passwd = builder.Password;
+
+            ConnectionInfo ci = new ConnectionInfo
+            {
+                ServerName = server,
+                DatabaseName = database,
+                UserID = user,
+                Password = passwd
+            };
+
+            ApplyConn(cryRpt, ci);
+        }
+
+        private void ApplyConn(ReportDocument cryRpt, ConnectionInfo connectionInfo)
+        {
+            foreach (Table table in cryRpt.Database.Tables)
+            {
+                TableLogOnInfo tableLogOnInfo = table.LogOnInfo;
+                tableLogOnInfo.ConnectionInfo = connectionInfo;
+                table.ApplyLogOnInfo(tableLogOnInfo);
+            }
+        }
+    }
+}

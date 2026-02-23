f77fa8d4e883b0dba40d57acbb922c42fca64a2a
MartinaCanamares <martina.canamares@sarria.salesians.cat>
2026-02-24 00:37:28 +0100

    aa

---
+++ Filiations/frmFiliations.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Filiations
+{
+    partial class frmFiliations
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(1479, 485);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(1355, 485);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Size = new System.Drawing.Size(646, 50);
+            this.lblTitle.Text = "";
+            // 
+            // lblLog
+            // 
+            this.lblLog.Location = new System.Drawing.Point(635, 491);
+            // 
+            // frmFiliations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1665, 1032);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "frmFiliations";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+

3729b941a5faff098420d3acf471018d33909415
Damian Altamirano Ontiveros Rivero <daltamiranoontiveros@gmail.com>
2026-02-23 23:02:28 +0100

    refact: Diseño mejorado del  main y formulario base (#38)
    se han modificado las propiedades de diseño y los colores de la aplicación entera.

--- FormsDesigner/frmBase.Designer.cs
+++ FormsDesigner/frmBase.Designer.cs
@@ -34,7 +34,11 @@ namespace FormsDesigner
             this.btnUpdate = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblLog = new System.Windows.Forms.Label();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.xwCloseButton = new XWInheritedControls.XWCloseButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBase)).BeginInit();
+            this.pnlTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.xwCloseButton)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvBase
@@ -43,21 +47,22 @@ namespace FormsDesigner
             this.dgvBase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvBase.Location = new System.Drawing.Point(90, 493);
+            this.dgvBase.Location = new System.Drawing.Point(83, 472);
             this.dgvBase.Margin = new System.Windows.Forms.Padding(6);
             this.dgvBase.Name = "dgvBase";
             this.dgvBase.ReadOnly = true;
-            this.dgvBase.Size = new System.Drawing.Size(1482, 449);
+            this.dgvBase.Size = new System.Drawing.Size(1359, 430);
             this.dgvBase.TabIndex = 0;
             // 
             // btnCreate
             // 
             this.btnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreate.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnCreate.Location = new System.Drawing.Point(1312, 439);
+            this.btnCreate.Location = new System.Drawing.Point(1203, 421);
             this.btnCreate.Margin = new System.Windows.Forms.Padding(6);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(124, 42);
+            this.btnCreate.Size = new System.Drawing.Size(114, 40);
             this.btnCreate.TabIndex = 2;
             this.btnCreate.Text = "Create";
             this.btnCreate.UseVisualStyleBackColor = true;
@@ -65,11 +70,12 @@ namespace FormsDesigner
             // btnUpdate
             // 
             this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnUpdate.Location = new System.Drawing.Point(1448, 439);
+            this.btnUpdate.Location = new System.Drawing.Point(1327, 421);
             this.btnUpdate.Margin = new System.Windows.Forms.Padding(6);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(124, 42);
+            this.btnUpdate.Size = new System.Drawing.Size(114, 40);
             this.btnUpdate.TabIndex = 1;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
@@ -78,10 +84,10 @@ namespace FormsDesigner
             // 
             this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(562, 28);
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(511, 43);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(539, 33);
+            this.lblTitle.Size = new System.Drawing.Size(494, 50);
             this.lblTitle.TabIndex = 3;
             this.lblTitle.Text = "Title";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -89,36 +95,64 @@ namespace FormsDesigner
             // lblLog
             // 
             this.lblLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLog.Location = new System.Drawing.Point(527, 446);
+            this.lblLog.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLog.Location = new System.Drawing.Point(483, 427);
             this.lblLog.Name = "lblLog";
-            this.lblLog.Size = new System.Drawing.Size(782, 33);
+            this.lblLog.Size = new System.Drawing.Size(717, 32);
             this.lblLog.TabIndex = 4;
             this.lblLog.Text = "Logs";
             this.lblLog.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             this.lblLog.Visible = false;
             // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.pnlTop.Controls.Add(this.xwCloseButton);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(1513, 40);
+            this.pnlTop.TabIndex = 5;
+            // 
+            // xwCloseButton
+            // 
+            this.xwCloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.xwCloseButton.HoverIconPath = "resources\\png\\CloseButtonShinyYellow.png";
+            this.xwCloseButton.ImageLocation = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\resourc" +
+    "es\\png\\CloseButtonYellow.png";
+            this.xwCloseButton.Location = new System.Drawing.Point(1470, 0);
+            this.xwCloseButton.MainIconPath = "resources\\png\\CloseButtonYellow.png";
+            this.xwCloseButton.Name = "xwCloseButton";
+            this.xwCloseButton.Size = new System.Drawing.Size(40, 37);
+            this.xwCloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.xwCloseButton.TabIndex = 0;
+            this.xwCloseButton.TabStop = false;
+            // 
             // frmBase
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(40)))), ((int)(((byte)(68)))));
-            this.ClientSize = new System.Drawing.Size(1650, 1010);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(29)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1513, 968);
+            this.Controls.Add(this.pnlTop);
             this.Controls.Add(this.lblLog);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.dgvBase);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "frmBase";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Base";
-            this.TopMost = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmBase_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvBase)).EndInit();
+            this.pnlTop.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.xwCloseButton)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -129,5 +163,7 @@ namespace FormsDesigner
         protected System.Windows.Forms.DataGridView dgvBase;
         protected System.Windows.Forms.Label lblTitle;
         protected System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Panel pnlTop;
+        private XWInheritedControls.XWCloseButton xwCloseButton;
     }
 }

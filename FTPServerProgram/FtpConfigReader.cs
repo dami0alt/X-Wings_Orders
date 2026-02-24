9d575df0860c38c1eb2b952172a8a618ffddd4a2
Axololt9597 <axel.fajardo@sarria.salesians.cat>
2026-02-24 22:24:49 +0100

    fix/frmFTP_Axel (#47)

--- FTPServerProgram/FtpConfigReader.cs
+++ FTPServerProgram/FtpConfigReader.cs
@@ -13,23 +13,23 @@ namespace FTPServerProgram
 	{
 		public static FtpConfig Load(string path)
 		{
-			XDocument doc = XDocument.Load(path);
+            XDocument doc = XDocument.Load(path);
+            XElement root = doc.Element("FtpConfig");
 
-			XElement root = doc.Element("FtpConfig");
+            string relativeLocal = root.Element("Folders").Element("Local").Value;
 
-			string relativeLocal = root.Element("Folders").Element("Local").Value;
+            string solutionRoot = Directory.GetParent(Application.StartupPath).Parent.Parent.FullName;
 
-			string absoluteLocal = Path.Combine(Application.StartupPath, relativeLocal);
+            string absoluteLocal = Path.Combine(solutionRoot, "UDP_Dark_Core", "X-Wings_Orders", "App", "FTP");
 
-			return new FtpConfig
-			{
-				Host = root.Element("ip").Value,
-				User = root.Element("Credentials").Element("UserName").Value,
-				Pass = root.Element("Credentials").Element("Password").Value,
-				RemoteRoot = root.Element("Folders").Element("RemoteRoot").Value,
-				ProcessedFolder = root.Element("Folders").Element("Processed").Value,
-				LocalFolder = root.Element("Folders").Element("Local").Value
-			};
-		}
+            return new FtpConfig
+            {
+                Host = root.Element("ip").Value,
+                User = root.Element("Credentials").Element("UserName").Value,
+                Pass = root.Element("Credentials").Element("Password").Value,
+                RemoteRoot = root.Element("Folders").Element("RemoteRoot").Value.TrimEnd('/'),
+                LocalFolder = absoluteLocal
+            };
+        }
 	}
 }
--- FTPServerProgram/FtpService.cs
+++ FTPServerProgram/FtpService.cs
@@ -21,63 +21,41 @@ namespace FTPServerProgram
 		// 🔹 Listar archivos del FTP
 		public string[] ListFiles()
 		{
-			FtpWebRequest request = (FtpWebRequest)
+			var request = (FtpWebRequest)
 				WebRequest.Create($"ftp://{Config.Host}{Config.RemoteRoot}");
 
 			request.Method = WebRequestMethods.Ftp.ListDirectory;
 			request.Credentials = new NetworkCredential(Config.User, Config.Pass);
 
-			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-
-			Stream responseStream = response.GetResponseStream();
-			StreamReader reader = new StreamReader(responseStream);
-
-			string data = reader.ReadToEnd();
-
-			reader.Close();
-			response.Close();
-
-			return data.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+			using (var response = (FtpWebResponse)request.GetResponse())
+			using (var reader = new StreamReader(response.GetResponseStream()))
+			{
+				string data = reader.ReadToEnd();
+				return data.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+			}
 		}
 
 		public void DownloadFile(string fileName)
 		{
 			string ftpUrl = $"ftp://{Config.Host}{Config.RemoteRoot}/{fileName}";
 
-			FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
-
+			var request = (FtpWebRequest)WebRequest.Create(ftpUrl);
 			request.Method = WebRequestMethods.Ftp.DownloadFile;
 			request.Credentials = new NetworkCredential(Config.User, Config.Pass);
 
-			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-			Stream responseStream = response.GetResponseStream();
-
-			if (!Directory.Exists(Config.LocalFolder))
-				Directory.CreateDirectory(Config.LocalFolder);
-
-			string localPath = Path.Combine(Config.LocalFolder, fileName);
-
-			using (FileStream fs = new FileStream(localPath, FileMode.Create))
+			using (var response = (FtpWebResponse)request.GetResponse())
+			using (var responseStream = response.GetResponseStream())
 			{
-				responseStream.CopyTo(fs);
+				if (!Directory.Exists(Config.LocalFolder))
+					Directory.CreateDirectory(Config.LocalFolder);
+
+				string localPath = Path.Combine(Config.LocalFolder, fileName);
+
+				using (var fs = new FileStream(localPath, FileMode.Create))
+				{
+					responseStream.CopyTo(fs);
+				}
 			}
-
-			response.Close();
-		}
-
-		public void MoveToProcessed(string fileName)
-		{
-			string ftpUrl = $"ftp://{Config.Host}{Config.RemoteRoot}/{fileName}";
-
-			FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
-
-			request.Method = WebRequestMethods.Ftp.Rename;
-			request.Credentials = new NetworkCredential(Config.User, Config.Pass);
-
-			request.RenameTo = $"{Config.ProcessedFolder}/{fileName}";
-
-			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-			response.Close();
 		}
 
 	}
